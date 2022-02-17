84f25eed31996e89d5abae7871a06043866cdf42
madoka <madokamiws@163.com>
2022-02-17 19:06:42 +0800

    自动眨眼 嘴部动画适应声音

--- Assets/Scripts/CubismHitTest.cs
+++ Assets/Scripts/CubismHitTest.cs
@@ -13,11 +13,12 @@ public class CubismHitTest : MonoBehaviour
         {
             return;
         }
-        CubismRaycaster cubismRaycaster = GetComponent<CubismRaycaster>();
+        CubismRaycaster cubismRaycaster = GetComponent<CubismRaycaster>(); //获取CubismRaycaster组件
         CubismRaycastHit[] cubismRaycastHits = new CubismRaycastHit[4];
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//射线
-        int hitCount = cubismRaycaster.Raycast(ray,cubismRaycastHits);//射线检测
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//获得射线
+        int hitCount = cubismRaycaster.Raycast(ray,cubismRaycastHits);//射线检测 参数（射线，射线命中的对象返回的数组）
+
         string requestText = hitCount.ToString();
         for (int i = 0; i < hitCount; i++)
         {
--- Assets/Scripts/ParametersUpdata.cs
+++ Assets/Scripts/ParametersUpdata.cs
@@ -23,6 +23,9 @@ public class ParametersUpdata : MonoBehaviour
     {
         
     }
+    /// <summary>
+    /// 动画修改在update之后，lateupdate之前，所以放到LU中
+    /// </summary>
     private void LateUpdate()
     {
         timeCount += Time.deltaTime * 4;
