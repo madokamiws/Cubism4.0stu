178d6030fb74f5aaae46f9cea9f7eeb18dabd2f3
madoka <madokamiws@163.com>
2022-03-02 19:01:40 +0800

    241-240

---
+++ Assets/Scripts/CubismExampleController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Live2D.Cubism.Framework;
+
+public class CubismExampleController : MonoBehaviour,ICubismUpdatable
+{
+    public int executionOrder;
+    //执行顺序（调用顺序）
+    public int ExecutionOrder => executionOrder;
+
+    //public int ExecutionOrder()
+    //{ 
+    //return executionOrder;
+    //}
+
+
+    public bool NeedsUpdateOnEditing => false;
+
+    public bool HasUpdateController { get ; set ; }
+
+    public void OnLateUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
